30515626fbf6caaf9468267eb1c68f9d0ff735f0
ibrahim muhammad <ibrex29@gmail.com>
2024-07-02 22:52:51 +0100

    worked on footer

--- admin/footer.php
+++ admin/footer.php
@@ -1,6 +1,6 @@
 <div class="footer-wrap pd-20 mb-20 card-box">
 					Copyright © 2024-
 					<a href="https://nitda.gov.ng" target="_blank"
-						>NITDA</a
+						>NITDA ACADEMY</a
 					>
 				</div>
